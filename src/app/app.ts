c45aeb5556bf6898011da4fa610c9681410c5d13
unknown <molanddise@gmail.com>
2026-03-02 18:03:55 -0600

    Auth, Token, User Service file was created and wrote the method post to cotumer API login

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,21 +2,43 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
-import { ProductsComponent } from "./components/allproducts/products-component/products-component";
-import { NavComponent } from "./components/nav/nav-component/nav-component";
+import { ProductsComponent } from './components/allproducts/products-component/products-component';
+import { NavComponent } from './components/nav/nav-component/nav-component';
+
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 @Component({
   selector: 'app-root',
-  imports: [
-    FormsModule,
-    NavComponent,
-    ProductsComponent,
-    RouterOutlet
-],
+  imports: [FormsModule, NavComponent, ProductsComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
   protected readonly title = signal('my-store');
+
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  createUser() {
+    this.usersService
+      .create({
+        name: 'Sebas',
+        email: 'sebas@mail.com',
+        password: '1212',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
+  }
+
+  login() {
+    this.authService.login('sebas@mail.com', '1212')
+    .subscribe((rta) => {
+      console.log(rta.access_token);
+    });
+  }
 
   // imgParent = '';
   // showImg = true;
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+import { Auth } from '../models/auth.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+  private urlApi = `${environment.API_URL}/api/v1/auth`;
+  constructor(private _http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this._http.post<Auth>(`${this.urlApi}/login`, {
+      email,
+      password
+    });
+  }
+
+  profile() {
+    return this._http.get(`${this.urlApi}/login`);
+  }
+
 }
