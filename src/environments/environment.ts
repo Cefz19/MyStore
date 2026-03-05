53bd667b1542b3dd6b695dbce74fb7a43bd2f247
unknown <molanddise@gmail.com>
2026-03-05 17:47:02 -0600

    The paging folders were created and the URL was registered to work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
     production: true,
-    API_URL: 'https://api.escuelajs.co',
+    API_URL: 'https://damp-spire-59848.herokuapp.com',
 };
+
+
+// export const environment = {
+//     production: true,
+//     API_URL: 'https://api.escuelajs.co',
+// };
