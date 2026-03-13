4994a959470a3058052c842535c650fe651b93cf
unknown <molanddise@gmail.com>
2026-03-13 15:35:22 -0600

    Shared files were created to separate and share resources

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,59 +1,12 @@
 import { Routes } from '@angular/router';
 
-import { LayoutComponent } from './website/components/layout.component/layout.component';
-import { HomeComponent } from './website/pages/home/home.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { MyCartComponent } from './website/pages/my-cart/my-cart.component';
 import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { ProfileComponent } from './website/pages/profiles/profile.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { RegisterComponent } from './website/pages/registers/register.component';
-import { ProductDetailComponent } from './website/pages/product-detail.components/product-detail.component';
+
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full',
-      },
-      {
-        path: 'home',
-        component: HomeComponent,
-      },
-      {
-        path: 'category/:id',
-        component: CategoryComponent,
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent,
-      },
-      {
-        path: 'my-cart',
-        component: MyCartComponent,
-      },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent,
-      },
-      {
-        path: 'recovery',
-        component: RecoveryComponent,
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
-      },
-    ],
+    loadChildren: () => import('./website/website.routes').then((m) => m.webRoutes),
   },
   {
     path: 'mcms',
---
+++ src/app/website/website.routes.ts
@@ -0,0 +1,58 @@
+import { Routes } from '@angular/router';
+
+import { LayoutComponent } from './../website/components/layout.component/layout.component';
+import { HomeComponent } from './../website/pages/home/home.component';
+import { CategoryComponent } from './../website/pages/category/category.component';
+import { LoginComponent } from './../website/pages/login/login.component';
+import { MyCartComponent } from './../website/pages/my-cart/my-cart.component';
+import { ProfileComponent } from './../website/pages/profiles/profile.component';
+import { RecoveryComponent } from './../website/pages/recovery/recovery.component';
+import { RegisterComponent } from './../website/pages/registers/register.component';
+import { ProductDetailComponent } from './../website/pages/product-detail.components/product-detail.component';
+
+
+export const webRoutes: Routes =  [
+     {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'category/:id',
+        component: CategoryComponent,
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent,
+      },
+      {
+        path: 'my-cart',
+        component: MyCartComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'recovery',
+        component: RecoveryComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
+  },
+]
